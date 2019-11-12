2b4be4fbe299b3f44ec6ac997f0ca38b2eda6e98
Tess Neau <tessneau@gmail.com>
2019-11-11 22:35:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, string) {
+  return drivers.filter(name => name.toLowerCase() === string.toLowerCase())
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(name => name.slice(0,2) === string)
+}
+
+function matchName(drivers, string) {
+  return drivers.filter(obj => obj.name === string)
+}
